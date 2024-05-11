4372077ed86466167c47d5033140ad5b0c954652
JoaoVitorLima242 <jlima0500@gmail.com>
2024-05-11 15:45:26 -0300

    starting nodejs server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from 'http'
+import { randomUUID } from 'crypto'
+import { pipeline } from 'stream';
+import { createWriteStream } from 'fs';
+
+async function handler(req, res) {
+  const fileName = `file-${randomUUID()}.csv`
+
+  await pipeline(
+    req,
+    createWriteStream(fileName)
+  )
+
+  res.end('Upload success!')
+}
+
+http.createServer(handler)
+  .listen(3000, () => console.log('Server running on port: 3000'));
+
